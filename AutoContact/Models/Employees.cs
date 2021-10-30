0513fe05a214ec56701afb1912cf8dbb9152ac39
Taylor DesRoches <taylor.desroches@hotmail.com>
2021-10-30 09:12:36 -0400

    - Added Employees (plural) model as a list to store all employees. - Added Method in Employee Controller - Mechanic (which just returns a list of employees, same as index essentially, but it looked like the name had to be changed to work with the @Model annotations. - Accidentally deleted Employee.cs so re-added but no changes. - Updated styling of Mechanic page to match client page. - Changed Client profile submit button from "save change" to "save changes"

---
+++ AutoContact/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoContact.Models
+{
+    public partial class Employees
+    {
+        public List<Employee> EmployeeList { get; set; }
+
+        public Employees(List<Employee> emps)
+        {
+            this.EmployeeList = emps;
+        }
+    }
+}
