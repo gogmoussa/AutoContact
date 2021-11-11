b5abb039adaa8cccfbbec32f4d9baaaa568d8d1b
Rachael <rachhrin@gmail.com>
2021-11-11 18:06:29 -0500

    Category/Vendor Controllers

---
+++ AutoContact/Controllers/PartsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutoContact.Models;
+using AutoContact.Helpers;
+
+namespace AutoContact.Controllers
+{
+    public class PartsController : Controller
+    {
+        private readonly AutoContactContext _context;
+
+        public PartsController(AutoContactContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Parts 
+        public async Task<IActionResult> Index()
+        {
+            var autoContactContext = _context.Parts.Include(p => p.Vendor).Include(p => p.Category);
+            return View(await autoContactContext.ToListAsync());
+        }
+
+        // GET: Parts/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts
+                .Include(e => e.Vendor)
+                .Include(e => e.Category)
+                .FirstOrDefaultAsync(m => m.PartId == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // GET: Parts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PartId,Name,Description,VendorId,CostPrice,ReorderQty,EconomicOrderQty,QtyOnHand,QtyOnOrder,CategoryId")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(part);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(part);
+        }
+
+        // GET: Parts/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts.Include(x => x.Vendor).Include(x => x.Category).FirstOrDefaultAsync(p => p.PartId == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Parts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("PartId,Name,Description,VendorId,CostPrice,ReorderQty,EconomicOrderQty,QtyOnHand,QtyOnOrder,CategoryId")] Part part)
+        {
+            if (id != part.PartId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(part);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PartExists(part.PartId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(part);
+        }
+
+        // GET: Parts/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts
+                .Include(x => x.Vendor)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync(m => m.PartId == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var part = await _context.Parts.FindAsync(id);
+            _context.Parts.Remove(part);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PartExists(long id)
+        {
+            return _context.Parts.Any(e => e.PartId == id);
+        }
+    }
+}
+
